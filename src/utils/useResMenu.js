e645a79932e643a80d545c5f4d89698259bc1255
Satya Sai Thota <satya07thota@gmail.com>
2024-07-25 20:36:23 +0530

    Update useResMenu.js

--- src/utils/useResMenu.js
+++ src/utils/useResMenu.js
@@ -1,3 +1,25 @@
+// import { useEffect, useState } from "react";
+// import { MENU_URL } from "./constants";
+// const useResMenu = (resId) =>{
+//     const[resInfo,setresInfo]=useState(null);
+
+//     useEffect(()=>{
+//         fetchData();
+//     },[]);
+
+//     const fetchData = async ()=>{
+//         const data=await fetch(MENU_URL+resId);
+//         const json=await data.json();
+        
+//         setresInfo(json.data);          
+//     };
+
+//     return resInfo;
+
+// }
+
+// export default useResMenu;
+
 import { useEffect, useState } from "react";
 import { MENU_URL } from "./constants";
 const useResMenu = (resId) =>{
